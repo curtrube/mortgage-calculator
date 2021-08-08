0b1f9cb321591ee131a0f2ebdaf677aa627bf0ee
Curtis Rubeck <rubeckcurtis@gmail.com>
2021-08-07 23:47:40 -0500

    commiting to format computer

--- js/site.js
+++ js/site.js
@@ -1,3 +1,9 @@
-const helloWorld = () => {
-  alert('Hello World');
+const calculateMortgage = (event) => {
+  event.preventDefault();
+  const loanAmount = document.getElementById('loanAmount').value;
+  const interestRate = document.getElementById('interestRate').value;
+  const length = document.getElementById('mortgageLength').value;
+  console.log(
+    `The loan of ${loanAmount} has been approved at a rate of ${interestRate} for ${length}`
+  );
 };
